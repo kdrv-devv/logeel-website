e599db3c8b3bbbe90c18447088ff1bf9e066ca7a
kdrv dev <ffeodalik@gmail.com>
2025-05-22 09:23:40 +0500

    finish

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // faqat root-dagi index.html faylni ko'radi
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
